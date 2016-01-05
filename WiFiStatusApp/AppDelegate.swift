ed3564bbfcc9a786f3d00e17be7243858be1fab6
angelo <angelo@angelos-mbp.homenet.telecomitalia.it>
2016-01-05 14:25:36 +0100

    Initial Commit

---
+++ WiFiStatusApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WiFiStatusApp
+//
+//  Created by angelo on 05/01/16.
+//  Copyright © 2016 no-one. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
